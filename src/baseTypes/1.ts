8de3a45604d9d218593ea7be743fcebc43e6c992
Andrii Marchuk <andrii.marchuck@gmail.com>
2024-02-09 17:59:09 +0200

    finish baseTypes lesson

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,12 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let age: number = 50;
+let name: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: any;
+let callback = (a: number): number => {
+  return 100 + a;
+}; // А якщо не явно вказати в аргумент тип any - тип буде void для функції
 
 export {};
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,26 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Week {
+  Sun = "Sunday",
+  Mon = "Monday",
+  Tue = "Tuesday",
+  Wen = "Wednesday",
+  Thu = "Thursday",
+  Fri = "Friday",
+  Sat = "Saturday",
+}
+
+function isWeekend(day: Week): boolean {
+  switch (day) {
+    case "Saturday":
+      return true;
+    case "Sunday":
+      return true;
+
+    default:
+      return false;
+  }
+  
+}
