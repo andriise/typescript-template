56da2aecb9a498ed8a46e3919c47c4b0ccb9f0eb
Andrii Marchuk <andrii.marchuck@gmail.com>
2024-02-17 21:52:25 +0200

    finish homework of 4 generics

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,19 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+type Top = Pick<AllType, "name" | "color">;
+type Bottom = Pick<AllType, "position" | "weight">;
+
+function compare<T extends Top, U extends Bottom>(top: T, bottom: U) {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
